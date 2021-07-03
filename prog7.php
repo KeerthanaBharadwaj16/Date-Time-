94e7f89ba694a41b3d2e863fab91b3dbaa4b4f1a
Keerthanajitwta <83495546+Keerthanajitwta@users.noreply.github.com>
2021-07-03 23:34:09 +0530

    Add files via upload

---
+++ prog7.php
@@ -0,0 +1,24 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta http-equiv="refresh" content="1" />
+<style>
+p {
+color: yellow;
+font-size: 45px;position: absolute;
+top: 50%;
+left: 50%;
+transform: translate(-50%, -50%);
+}
+body {
+background-color: Maroon;
+}
+</style>
+<p> <?php echo "Server time is " . date(" h: i : s A") . "<br>";
+echo "Today is " . date("Y/m/d") . "<br>";
+echo "Today is " . date("Y.m.d") . "<br>";
+echo "Today is " . date("Y-m-d") . "<br>";
+echo "Today is " . date("l");
+?>
+</p></head>
+</html>
